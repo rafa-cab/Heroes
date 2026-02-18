04134f63c44cec1cadd7d96e325a3910b52891b1
Rafael Achutegui caballero <rafaelachuteguicaballero@gmail.com>
2026-02-18 20:59:45 +0100

    masfacil

--- Heroes.gestion/Heroes.Gestion/Gestion/Models/Misiones.cs
+++ Heroes.gestion/Heroes.Gestion/Gestion/Models/Misiones.cs
@@ -2,7 +2,16 @@ namespace Gestion.Models;
 
 public class Misiones
 {
+    public string ID  { get; set; }
+    public int Dificultad {get; set; }
 
-    
+    public enum EstadoMision
+    {
+    Pendiente, 
+    Completada
+        
+
+    }
+
 
 }
